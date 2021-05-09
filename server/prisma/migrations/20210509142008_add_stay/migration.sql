72aac611508ab68bc98687416a126c711722f3af
Makoto Hirose <makoto.hirose@vrg.jp>
2021-05-09 23:22:49 +0900

    feat: add stay table

---
+++ server/prisma/migrations/20210509142008_add_stay/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE `Stay` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `userId` INTEGER,
+    `countryId` INTEGER,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Stay` ADD FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Stay` ADD FOREIGN KEY (`countryId`) REFERENCES `Country`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
